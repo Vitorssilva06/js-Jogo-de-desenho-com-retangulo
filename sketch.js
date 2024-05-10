44b4cc8a4ec9bc313a319bcc7d5471c8faf320b9
Vitorssilva06 <vitorsoaress187@gmail.com>
2024-05-10 07:34:14 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+    
+    
+    stroke("red");
+    fill("white")
+    
+    //console.log(mouseIsPressed);
+    
+    if(mouseIsPressed) {
+    rect(mouseX, mouseY, 32
+    , 36);
+   }
+    
+    
+    
+    
+  }
